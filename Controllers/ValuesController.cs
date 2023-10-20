dce967ad67a8d9a557211d23b86e3f4112b0c744
nurayklc <nuraykilic0101@gmail.com>
2023-10-20 16:38:54 +0300

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name", name);
+        }
+
+        [HttpGet("get")]   
+        public string Get()
+        {
+            if(_memoryCache.TryGetValue<string>("name", out string name))
+            {
+                return name.Substring(2);
+            }
+            return "";
+        }
+    }
+}
