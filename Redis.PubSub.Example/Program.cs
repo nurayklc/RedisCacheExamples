aadc6d14dbfac5563b617911b66449f8f979aee7
nurayklc <nuraykilic0101@gmail.com>
2023-10-23 17:55:32 +0300

    Add redis pub sub

---
+++ Redis.PubSub.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Please enter your message!");
+    string message  = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel.*", message);
+}
