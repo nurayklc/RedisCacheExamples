9899e9e540e5dcb2a8d544efca33c6fe1a79f7e4
nurayklc <nuraykilic0101@gmail.com>
2023-10-26 11:46:30 +0300

    Add redis sentinel structure

---
+++ InMemory.Caching/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("Set")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name", name);
+        }
+
+        [HttpGet("get")]   
+        public string Get()
+        {
+            if(_memoryCache.TryGetValue<string>("name", out string name))
+            {
+                return name.Substring(2);
+            }
+            return "";
+        }
+
+        [HttpGet("SetDate")]
+        public void SetDate(string name)
+        {
+            _memoryCache.Set("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("GetDate")]   
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
---
+++ Redis.Sentinel/Services/RedisService.cs
@@ -0,0 +1,50 @@
+﻿using StackExchange.Redis;
+using System.Net;
+
+namespace Redis.Sentinel.Services
+{
+    public class RedisService
+    {
+        static ConfigurationOptions sentinelOptions => new()
+        {
+            EndPoints =
+            {
+                { "localhost", 6383 },
+                { "localhost", 6384 },
+                { "localhost", 6385 },
+            },
+            AbortOnConnectFail = false,
+            CommandMap = CommandMap.Sentinel
+        };
+
+        static ConfigurationOptions masterOptions => new()
+        {
+            AbortOnConnectFail = false
+        };
+
+        public static async Task<IDatabase> RedisMasterDatabase()
+        {
+            ConnectionMultiplexer sentinelConnection = await ConnectionMultiplexer.SentinelConnectAsync(sentinelOptions);
+
+            EndPoint masterEndPoint = null;
+            foreach (EndPoint endpoint in sentinelConnection.GetEndPoints())
+            {
+                IServer server = sentinelConnection.GetServer(endpoint);
+                if (!server.IsConnected)
+                    continue;
+                masterEndPoint = await server.SentinelGetMasterAddressByNameAsync("mymaster");
+                break; 
+            }
+
+            var localMasterIP = masterEndPoint.ToString() switch
+            {
+                "172.17.0.3:6379" => "localhost:6379",
+                "172.17.0.4:6379" => "localhost:6379"
+            };
+
+            ConnectionMultiplexer masterConnection = await ConnectionMultiplexer.ConnectAsync(localMasterIP);
+            IDatabase database = masterConnection.GetDatabase();
+            return database;
+        }
+    }
+}
